829f7d646f3e09fbe168a535b22c05faab40832a
Yan Cheng <plasic@gmail.com>
2025-07-19 22:53:39 -0700

    day 3 completed - this is a functional version

--- javascript-course/tic-tac-toe/script.js
+++ javascript-course/tic-tac-toe/script.js
@@ -42,10 +42,18 @@ function Game() {
   return {
     matrix: Array(9).fill(""),
     currentPlayer: "X",
+    gameWinner: "",
+    gameCompleted: false,
+    playCount: 0,
 
     createBoard() {
       const boardDiv = document.querySelector("#board");
       this.matrix = Array(9).fill("");
+      this.currentPlayer = "X";
+      this.gameWinner = "";
+      this.gameCompleted = false;
+      this.playCount = 0;
+
       // boardDiv.innerHTML = "";
       for (let i = 0; i < 9; i++) {
         let newDiv = document.createElement("div");
@@ -53,9 +61,11 @@ function Game() {
         newDiv.dataset.index = i;
         newDiv.innerHTML = `${this.matrix[i]}`;
         newDiv.addEventListener("click", () => {
-          if (this.matrix[i] === "") {
+          if (this.matrix[i] === "" && !this.gameCompleted) {
             this.matrix[i] = this.currentPlayer;
             newDiv.innerHTML = `${this.matrix[i]}`;
+            this.checkWin(i);
+            this.playCount++;
             this.togglePlayer();
           }
         });
@@ -73,13 +83,49 @@ function Game() {
         "#current-user"
       ).innerHTML = `Current User: ${this.currentPlayer}`;
     },
+
+    checkWin() {
+      const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      winConditions.forEach((item) => {
+        if (
+          this.matrix[item[0]] === this.matrix[item[1]] &&
+          this.matrix[item[1]] === this.matrix[item[2]] &&
+          this.matrix[item[0]] !== ""
+        ) {
+          this.gameWinner = this.matrix[item[0]];
+          this.gameCompleted = true;
+          document.querySelector(
+            "#game-result"
+          ).textContent = `Game Result: ${this.gameWinner} wins!`;
+          return;
+        }
+      });
+
+      if (this.gameWinner === "" && this.playCount === 8) {
+        this.gameWinner = "draw";
+        document.querySelector(
+          "#game-result"
+        ).textContent = `Game Result: ${this.gameWinner}`;
+      }
+    },
   };
 }
 
-// const game = Game()
-Game().createBoard();
+const game = Game();
+game.createBoard();
 
-// document.querySelector("#restart-game").addEventListener("click", () => {
-//   game = Game();
-//   game.createBoard();
-// });
+document.querySelector("#restart-game").addEventListener("click", () => {
+  const boardDiv = document.querySelector("#board");
+  boardDiv.innerHTML = "";
+  game.createBoard();
+});
